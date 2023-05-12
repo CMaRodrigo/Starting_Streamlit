6e8ac0bb14322c786d516c53405a3bde7923854f
Rodrigo da Rosa <Rodrigo.Rosa001@edu.pucrs.br>
2023-05-12 16:27:08 -0300

    aa

--- stream.py
+++ stream.py
@@ -4,5 +4,16 @@ option = st.selectbox(
     'What database are you working on?',
     ('Sicredi', 'Banrisul', 'Cotrirosa', 'Circulo', 'São José', 'Sinoserra'))
 
-st.write('You selected:', option)
+col1, col2 = st.columns(2)
+
+with col2:
+    text_input = st.text_input(
+        "Digite o Caminho do Arquivo (C://) 👇",
+        label_visibility=st.session_state.visibility,
+        disabled=st.session_state.disabled,
+        placeholder=st.session_state.placeholder,
+    )
+    if text_input:
+        st.write("You entered: ", text_input)
+
 
