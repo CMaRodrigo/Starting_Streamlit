f8389425fd3c995e518ed88eef37c80aa5bf6dde
Rodrigo da Rosa <Rodrigo.Rosa001@edu.pucrs.br>
2023-05-12 16:41:09 -0300

    aa

--- stream.py
+++ stream.py
@@ -3,9 +3,10 @@ import pandas as pd
 
 option = st.selectbox(
     'What database are you working on?',
-    ('Sicredi', 'Banrisul', 'Cotrirosa', 'Circulo', 'São José', 'Sinoserra'))
+    ('Sicredi', 'Banrisul', 'Cotrirosa', 'Circulo', 'São José', 'Sinoserra')
+)
 
-col1 = st.columns(2)
+col1, _ = st.columns(2)
 
 with col1:
     text_input = st.text_input(
@@ -13,9 +14,7 @@ with col1:
     )
     if text_input:
         st.write("You entered: ", text_input)
-
-
-
-
-df = pd.read_excel("T:/Dados/Analytics/1. Projetos/18. Sicredi/Picsel/Precificação da carteira/consolidada.xlsx")
-st.dataframe(df)  
+        
+    if st.button('Load data'):
+        df = pd.read_excel(text_input)
+        st.dataframe(df)
