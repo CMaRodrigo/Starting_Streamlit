c71c5b4dee5a058b0ea4d2cc6088a3463f96c900
Rodrigo da Rosa <Rodrigo.Rosa001@edu.pucrs.br>
2023-05-12 16:16:38 -0300

    atualizando e acordo com o projeto

--- stream.py
+++ stream.py
@@ -1,38 +1,8 @@
 import streamlit as st
 
-#Teste
+def base():
+    option = st.selectbox(
+        'What database are you working on?',
+        ('Sicredi', 'Banrisul', 'Cotrirosa', 'Circulo', 'São José'))
+    st.write('You selected:', option)
 
-if st.button('Say hello'):
-    st.write('Why hello there')
-else:
-    st.write('Goodbye')
-
-#----------
-
-option = st.selectbox(
-    'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone'))
-
-st.write('You selected:', option)
-
-#--------------
-
-genre = st.radio(
-    "What\'s your favorite movie genre",
-    ('Comedy', 'Drama', 'Documentary'))
-
-if genre == 'Comedy':
-    st.write('You selected comedy.')
-else:
-    st.write("You didn\'t select comedy.")
-
-#---------
-
-img_file_buffer = st.camera_input("Take a picture")
-
-if img_file_buffer is not None:
-    # To read image file buffer as bytes:
-    bytes_data = img_file_buffer.getvalue()
-    # Check the type of bytes_data:
-    # Should output: <class 'bytes'>
-    st.write(type(bytes_data))
