6ba04f53c242179c6e8f570621d8bde73c09b842
Rodrigo da Rosa <Rodrigo.Rosa001@edu.pucrs.br>
2023-05-12 16:40:13 -0300

    aa

--- stream.py
+++ stream.py
@@ -1,14 +1,21 @@
 import streamlit as st
+import pandas as pd
 
 option = st.selectbox(
     'What database are you working on?',
     ('Sicredi', 'Banrisul', 'Cotrirosa', 'Circulo', 'São José', 'Sinoserra'))
 
-col1, col2 = st.columns(2)
+col1 = st.columns(2)
 
-with col2:
+with col1:
     text_input = st.text_input(
-        "Digite o Caminho do Arquivo (C://) 👇"
+        "Digite o Caminho do Arquivo (C:/) 👇"
     )
     if text_input:
         st.write("You entered: ", text_input)
+
+
+
+
+df = pd.read_excel("T:/Dados/Analytics/1. Projetos/18. Sicredi/Picsel/Precificação da carteira/consolidada.xlsx")
+st.dataframe(df)  
