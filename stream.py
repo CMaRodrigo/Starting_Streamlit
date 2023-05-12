677f73d54ccacfb7602b86b79703607a3265deaf
Rodrigo da Rosa <Rodrigo.Rosa001@edu.pucrs.br>
2023-05-12 16:19:40 -0300

    arrumando

--- stream.py
+++ stream.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-def base():
-    option = st.selectbox(
-        'What database are you working on?',
-        ('Sicredi', 'Banrisul', 'Cotrirosa', 'Circulo', 'São José'))
-    st.write('You selected:', option)
+option = st.selectbox(
+    'What database are you working on?',
+    ('Sicredi', 'Banrisul', 'Cotrirosa', 'Circulo', 'São José', 'Sinoserra'))
+
+st.write('You selected:', option)
 
