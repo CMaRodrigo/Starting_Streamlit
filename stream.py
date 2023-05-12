89ff0211ec72daac43908f51e22d0ba014e5ee7a
Rodrigo da Rosa <Rodrigo.Rosa001@edu.pucrs.br>
2023-05-12 17:59:16 -0300

    a

--- stream.py
+++ stream.py
@@ -7,22 +7,18 @@ st.title('Read a file using Pandas')
 # Allow the user to select a file from their local file system
 file = st.file_uploader("Choose a file")
 
-# If the user selected a file and clicked the "Read" button
-if st.button('Read'):
-    # Load the workbook using Openpyxl
-    wb = openpyxl.load_workbook('example.xlsx')
+# Load the workbook using Openpyxl
+wb = openpyxl.load_workbook(file)
 
-        # Select the sheet to read
-    sheet = wb['seguro']
+# Select the sheet to read
+sheet = wb['seguro']
 
-        # Read the sheet into a list of lists
-    rows = sheet.values
-    data = []
-    for row in rows:
-        data.append(list(row))
+# Read the sheet into a list of lists
+rows = sheet.values
+data = []
+for row in rows:
+    data.append(list(row))
 
 # Create a Pandas dataframe from the data
 df = pd.DataFrame(data[1:], columns=data[0])
 st.write(df)
-
-
